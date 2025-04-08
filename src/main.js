b3806b10b7fc59313c1a6202184ea64976fc09fb
kabeersingh77 <kabeersing337@gmail.com>
2025-04-08 23:20:33 +0530

    made the program and UI

--- src/main.js
+++ src/main.js
@@ -1,24 +1,63 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.getElementById("generate-form");
+const qr = document.getElementById("qrcode");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const onGenerateSubmit = (e) => {
+  e.preventDefault();
 
-setupCounter(document.querySelector('#counter'))
+  clearUI();
+
+  const url = document.getElementById('url').value;
+  const size = document.getElementById('size').value;
+
+  if (url === '') {
+    alert('Please enter a URL');
+  }else{
+    showLoader();
+
+    setTimeout(() => {
+      hideLoader();
+
+      generateQRCode(url, size);
+
+      setTimeout(() => { 
+        const saveUrl = qr.querySelector('img').src;
+        saveTheQR(saveUrl);
+      }, 50);
+    }, 1000);
+  }
+};
+
+const generateQRCode = (url, size) => {
+  const qrcode = new QRCode('qrcode', {
+    text: url,
+    width: size,
+    height: size,
+  });
+};
+
+const showLoader = () => {
+  document.getElementById('loader').style.display = 'block';
+};
+const hideLoader = () => {
+  document.getElementById('loader').style.display = 'none';
+};
+
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if(saveLink) saveLink.remove();
+};
+
+const saveTheQR = (saveUrl) => {
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList = 'bg-gray-300 hover:bg-slate-500 text-black font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = 'Save Image';
+  document.getElementById('generated').appendChild(link);
+};
+
+hideLoader();
+
+form.addEventListener('submit', onGenerateSubmit);
